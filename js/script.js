94f4b1fa76f08a8e4ff2b0fc9b4d2b9963077a2b
Barov Denis <barovdenis@mail.ru>
2022-02-24 18:29:31 +0300

    js-second

--- js/script.js
+++ js/script.js
@@ -35,3 +35,30 @@ const cart = () => {
 }
 
 cart();
+
+
+// Работа с данными
+
+const getGoods = () => {
+
+  const links = document.querySelectorAll('.navigation-link');
+
+  const getData = () => {
+    fetch('/e-shop/db/db.json')
+    .then((res) => res.json())
+      .then((data) => {
+        localStorage.setItem('data', JSON.stringify(data));
+        console.log(data);
+  })
+  }
+  
+  links.forEach(link => {   
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      getData();
+    })
+  });
+}
+
+
+getGoods();
